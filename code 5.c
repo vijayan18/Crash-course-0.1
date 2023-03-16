4ad332dd13e799242682b8743a534dae1b0d1aed
vijayan18 <128016794+vijayan18@users.noreply.github.com>
2023-03-16 11:57:16 +0530

    Create code 5.c

---
+++ code 5.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main()
+{
+    for(int a = 98701;a > 0; a/=10 ){
+        printf("%d" , a%10 );}
+    return 0;    
+    }
+    
